b47118473191e5bd6d6b66c4354a415bb2bd499e
krasm <mkrasowski@gmail.com>
2021-12-11 15:38:24 +0000

    second

---
+++ test/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void justFail() {
+        fail("nothing is tested here");
+    }
+
+}
